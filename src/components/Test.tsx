fd007303105439089b8728bae62b57ac21a6a9e1
Evgeniy Galitsin <ESGalitsyn@sberbank.ru>
2020-05-30 00:03:00 +0300

    added test file

---
+++ src/components/Test.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import {
+  StyledPhotoCardContainer,
+  StyledPhotoCard,
+} from "../styles";
+import {StyledCloseButton} from '../styles'
+
+export interface IPhotoCardProps {
+  onClose: () => void;
+  onClick: () => void;
+  src: string;
+}
+
+const PhotoCard: FC<IPhotoCardProps> = ({ onClick, onClose, src }) => {
+  return (
+    <StyledPhotoCardContainer>
+      <StyledCloseButton onClick={onClose}/>
+      <StyledPhotoCard onClick={onClick} src={src} />
+    </StyledPhotoCardContainer>
+  );
+};
+
+export { PhotoCard };
